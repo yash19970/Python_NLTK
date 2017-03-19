76c55f851710caf14f3684ee598e6dfd0b2aa7af
yash19970 <yash_19@outlook.com>
2017-03-20 00:41:36 +0530

    classifier completed.

--- textclassifier.py
+++ textclassifier.py
@@ -1,107 +1,154 @@
-# text classifier for sentimental analysis.
-import nltk,pickle
+from scipy.stats import mode
+import nltk
 import random
-from nltk.corpus import movie_reviews
+#from nltk.corpus import movie_reviews
 from nltk.classify.scikitlearn import SklearnClassifier
-from sklearn.naive_bayes import MultinomialNB,GaussianNB, BernoulliNB
-from sklearn.linear_model import LogisticRegression,SGDClassifier
+import pickle
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 from nltk.classify import ClassifierI
-from scipy.stats import mode
+from nltk.tokenize import word_tokenize
+
+
 
 class VoteClassifier(ClassifierI):
-	def __init__(self, *classifiers):
-		self._classifiers = classifiers
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
 
-	def classify(self,features):
-		votes = []
-		for c in self._classifiers:
-			v = c.classify(features)
-			votes.append(v)
-		return mode(votes)
-	def confidence(self,features):
-		votes = []
-		for c in self._classifiers:
-			v = c.classify(features)
-			votes.append(v)
-		choice_votes  = votes.count(mode(votes))
-		conf = choice_votes / len(votes) #certainty.
-		return conf
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
 
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
 
-documents = [(list(movie_reviews.words(fileid)),category)
-			for category in movie_reviews.categories()
-			for fileid in movie_reviews.fileids(category)]
-random.shuffle(documents)
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+    
+short_pos = open("short_reviews/positive.txt","r").read()
+short_neg = open("short_reviews/negative.txt","r").read()
 
+# move this up here
 all_words = []
-for w in movie_reviews.words():
-	w = w.lower()
-	all_words.append(w)
-print '\n'
-# convert to nltk freq distribution
+documents = []
+
+
+#  j is adject, r is adverb, and v is verb
+#allowed_word_types = ["J","R","V"]
+allowed_word_types = ["J"]
+
+for p in short_pos.split('\n'):
+    documents.append( (p, "pos") )
+    words = word_tokenize(p)
+    pos = nltk.pos_tag(words)
+    for w in pos:
+        if w[1][0] in allowed_word_types:
+            all_words.append(w[0].lower())
+
+    
+for p in short_neg.split('\n'):
+    documents.append( (p, "neg") )
+    words = word_tokenize(p)
+    pos = nltk.pos_tag(words)
+    for w in pos:
+        if w[1][0] in allowed_word_types:
+            all_words.append(w[0].lower())
+
+
+
+save_documents = open("pickled_algos/documents.pickle","wb")
+pickle.dump(documents, save_documents)
+save_documents.close()
+
+
 all_words = nltk.FreqDist(all_words)
-# TRAINING DATASET.
-word_features = list(all_words.keys())[:3000]
+
+
+word_features = list(all_words.keys())[:5000]
+
+
+save_word_features = open("pickled_algos/word_features5k.pickle","wb")
+pickle.dump(word_features, save_word_features)
+save_word_features.close()
+
+
 def find_features(document):
-	words = set(document)
-	features = {}
-	for w in word_features:
-		features[w] = (w in words)
-	return features
-#print find_features(movie_reviews.words('neg/cv000_29416.txt'))
-feature_sets = [(find_features(rev), category) for (rev,category) in documents]
+    words = word_tokenize(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
 
-#NAIVE BAIS ALGO.
-training_set = feature_sets[:1900]
-testing_set = feature_sets[1900:]
-#classifier  = nltk.NaiveBayesClassifier.train(training_set)
+    return features
 
-classifier_file = open("naivebayes.pickle","rb")
-classifier  = pickle.load(classifier_file)
-classifier_file.close()
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
-print nltk.classify.accuracy(classifier,training_set)*100
+random.shuffle(featuresets)
+print(len(featuresets))
+
+testing_set = featuresets[10000:]
+training_set = featuresets[:10000]
+
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
-#using pickle to save python objects. (save classifier)
 
+###############
+save_classifier = open("pickled_algos/originalnaivebayes5k.pickle","wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
 
-#MULTINOMIAL NB: EXAMPLES.
 MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(training_set)
-print 'MNB Classifier: \n'
-print nltk.classify.accuracy(MNB_classifier,testing_set)*100
+print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+save_classifier = open("pickled_algos/MNB_classifier5k.pickle","wb")
+pickle.dump(MNB_classifier, save_classifier)
+save_classifier.close()
 
 BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
 BernoulliNB_classifier.train(training_set)
-print 'BernoulliNB Classifier: \n'
-print nltk.classify.accuracy(BernoulliNB_classifier,testing_set)*100
+print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+save_classifier = open("pickled_algos/BernoulliNB_classifier5k.pickle","wb")
+pickle.dump(BernoulliNB_classifier, save_classifier)
+save_classifier.close()
 
 LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
-print 'LogisticRegression Classifier: \n'
-print nltk.classify.accuracy(LogisticRegression_classifier,testing_set)*100
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
 
-SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
-SGDClassifier_classifier.train(training_set)
-print 'SGDClassifier Classifier: \n'
-print nltk.classify.accuracy(SGDClassifier_classifier,testing_set)*100
+save_classifier = open("pickled_algos/LogisticRegression_classifier5k.pickle","wb")
+pickle.dump(LogisticRegression_classifier, save_classifier)
+save_classifier.close()
+
 
 LinearSVC_classifier = SklearnClassifier(LinearSVC())
 LinearSVC_classifier.train(training_set)
-print 'LinearSVC Classifier: \n'
-print nltk.classify.accuracy(LinearSVC_classifier,testing_set)*100
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
 
-NuSVC_classifier = SklearnClassifier(NuSVC())
-NuSVC_classifier.train(training_set)
-print 'NuSVC Classifier: \n'
-print nltk.classify.accuracy(NuSVC_classifier,testing_set)*100
+save_classifier = open("pickled_algos/LinearSVC_classifier5k.pickle","wb")
+pickle.dump(LinearSVC_classifier, save_classifier)
+save_classifier.close()
 
-#NEW CLASSIFIER with voting.
 
-voted_classifier  = VoteClassifier(classifier,SGDClassifier_classifier,MNB_classifier,BernoulliNB_classifier,NuSVC_classifier,LinearSVC_classifier)
-print 'Voted Classifier accuracy percent: \n'
-print nltk.classify.accuracy(voted_classifier,testing_set)*100
+##NuSVC_classifier = SklearnClassifier(NuSVC())
+##NuSVC_classifier.train(training_set)
+##print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
 
-#print 'Classification:',voted_classifier.classify(testing_set[0][0]),'\n confidence:',voted_classifier.confidence(testing_set[0][0])
 
+SGDC_classifier = SklearnClassifier(SGDClassifier())
+SGDC_classifier.train(training_set)
+print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
+
+save_classifier = open("pickled_algos/SGDC_classifier5k.pickle","wb")
+pickle.dump(SGDC_classifier, save_classifier)
+save_classifier.close()
